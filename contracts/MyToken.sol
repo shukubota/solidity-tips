5189fc4492cdbed40a91f90a1dec6f80aecde841
kubotashu0421@gmail.com <kubotashu0421@gmail.com>
2022-01-30 21:04:26 +0900

    lint修正

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,18 +1,18 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
   struct LastSpent {
-    from;
-    to;
-    amount;
+    address from;
+    address to;
+    uint256 amount;
   }
   LastSpent private _last;
 
   constructor(string memory name, string memory symbol) ERC20(name, symbol) public {}
 
-  function _beforeTokenTransfer (address: from, address: to, uint256: amount) internal virtual override {
+  function _beforeTokenTransfer (address from, address to, uint256 amount) internal virtual override {
     _last.from = from;
     _last.to = to;
     _last.amount = amount;

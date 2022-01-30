fa6d55da82272ad01240dfdd027885179d2e46f1
kubotashu0421@gmail.com <kubotashu0421@gmail.com>
2022-01-30 20:59:44 +0900

    contractsディレクトリへ移動

---
+++ contracts/MyToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.11;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+  struct LastSpent {
+    from;
+    to;
+    amount;
+  }
+  LastSpent private _last;
+
+  constructor(string memory name, string memory symbol) ERC20(name, symbol) public {}
+
+  function _beforeTokenTransfer (address: from, address: to, uint256: amount) internal virtual override {
+    _last.from = from;
+    _last.to = to;
+    _last.amount = amount;
+  }
+}

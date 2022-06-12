ca01677d339f4ecabddee61eaf914369469068ed
shukubota <kubotashu0421@gmail.com>
2022-06-12 21:59:15 +0900

    sample sol

---
+++ solidity-playground/singleNumRegister.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+contract SingleNumRegister {
+    uint storedData;
+    function set(uint x) public{
+        storedData = x;
+    }
+    function get() public view returns (uint retVal){
+        return storedData;
+    }
+}

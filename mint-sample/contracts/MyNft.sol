c5b31f4aab2f3423d9eb8222b85e05bf84f911c8
shukubota <kubotashu0421@gmail.com>
2023-01-08 20:38:33 +0900

    url修正

--- mint-sample/contracts/MyNft.sol
+++ mint-sample/contracts/MyNft.sol
@@ -4,6 +4,6 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 contract MyNFT is ERC721PresetMinterPauserAutoId {
     constructor()
-        ERC721PresetMinterPauserAutoId("MYNFT", "nft", "https://s3.console.aws.amazon.com/s3/buckets/mint-sample")
+        ERC721PresetMinterPauserAutoId("MYNFT", "nft", "https://mint-sample.s3.us-east-2.amazonaws.com/")
     {}
 }

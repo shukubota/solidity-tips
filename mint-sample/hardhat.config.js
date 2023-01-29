7aa0ddbfb6d91ce74d6a1df87b8fe6f69894ec04
shukubota <kubotashu0421@gmail.com>
2023-01-29 18:59:27 +0900

    config修正

--- mint-sample/hardhat.config.js
+++ mint-sample/hardhat.config.js
@@ -7,5 +7,13 @@ module.exports = {
       chainId: 31337,
       url: "http://127.0.0.1:8545",
     },
+    // goerli: {
+    //   url: "https://eth-goerli.g.alchemy.com/v2/H4aiCCamXTXUdzj2_u5Bth8eBVYlmGcy",
+    //   accounts: ["ここに秘密鍵"],
+    // },
+    // mainnet: {
+    //   url: "Alchemyで得たEthereumのmainnetのURL",
+    //   accounts: "自分のウォレットの秘密鍵",
+    // },
   },
 };
